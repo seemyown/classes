f42918f83dcf617e1acf28d5a1245514f98e1aa6
seemyown <semyon.albeev@gmail.com>
2023-03-31 11:29:01 +0300

    Task_2
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   homework_1.py

--- homework_1.py
+++ homework_1.py
@@ -6,13 +6,35 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def grade_lecture(self, lecture, course, grade):
+        if isinstance(lecture, Lecture) and course in lecture.courses_attached and course in self.courses_in_progress:
+            if course in lecture.grades:
+                lecture.grades[course] += [grade]
+            else:
+                lecture.grades[course] = [grade]
+        else:
+            return 'Ошибка'
         
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+        self.grades = {}
+    
         
+class Lecture(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_attached = []
+        self.grades = {}
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_attached = []
+
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -21,26 +43,31 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-        
-class Lecture(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-
-
-class Reviewer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
 
         
  
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python']
  
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+# cool_mentor = Mentor('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python']
  
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
  
-print(best_student.grades)
+# print(best_student.grades)
+
+student = Student('Jack', 'Sparrow', 'men')
+student.courses_in_progress += ['Python']
+# print(student.__dict__)
+reviewer = Reviewer('John', 'Wall')
+reviewer.courses_attached += ['Python']
+
+lecture = Lecture('Leo', 'Dicaprio')
+lecture.courses_attached += ['Python']
+# print(lecture.__dict__)
+reviewer.rate_hw(student, 'Python', 10)
+student.grade_lecture(lecture, 'Python', 8)
+print(student.__dict__)
+print(lecture.__dict__)

e3a160ca0cc7db39cb4edd1d31e528ab7dcefd17
seemyown <semyon.albeev@gmail.com>
2023-04-02 12:14:16 +0300

    Task_4
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   homework_1.py

--- homework_1.py
+++ homework_1.py
@@ -80,3 +80,64 @@ class Reviewer(Mentor):
     def __str__(self) -> str:
         return f'Имя: {self.name}\nФамилия: {self.surname}'
  
+
+def aversge_grade_students(students_list, course):
+    grades = []
+    for student in students_list:
+        if course in student.courses_in_progress:
+            grades.append(student.average())
+    return f'Средняя оценка студентов по курсу "{course}": {sum(grades)/len(grades)}'
+
+def aversge_grade_lectures(lectures_list, course):
+    grades = []
+    for lecture in lectures_list:
+        if course in lecture.courses_attached:
+            grades.append(lecture.average())
+    return f'Средняя оценка лекторов по курсу "{course}": {sum(grades)/len(grades)}'
+
+
+student_1 = Student('Pol', 'Doll', 'male')
+student_2 = Student('Kate', 'Rill', 'female')
+student_1.courses_in_progress += ['Python']
+student_2.courses_in_progress += ['Python']
+
+lecture_1 = Lecture('Dmitriy', 'Corush')
+lecture_2 = Lecture('Anna', 'Dell')
+lecture_1.courses_attached += ['Python']
+lecture_2.courses_attached += ['Python']
+
+student_1.grade_lecture(lecture_1, 'Python', 10)
+student_2.grade_lecture(lecture_1, 'Python', 9)
+student_1.grade_lecture(lecture_1, 'Python', 9)
+student_2.grade_lecture(lecture_1, 'Python', 10)
+
+student_1.grade_lecture(lecture_2, 'Python', 10)
+student_2.grade_lecture(lecture_2, 'Python', 9)
+student_1.grade_lecture(lecture_2, 'Python', 9)
+student_2.grade_lecture(lecture_2, 'Python', 10)
+
+reviewer_1 = Reviewer('Leo', 'Dicaprio')
+reviewer_2 = Reviewer('Jack', 'Sparrow')
+reviewer_1.courses_attached += ['Python', 'Java']
+reviewer_2.courses_attached += ['Java', 'Python']
+
+reviewer_1.rate_hw(student_1, 'Python', 10)
+reviewer_1.rate_hw(student_1, 'Python', 9)
+reviewer_1.rate_hw(student_1, 'Python', 8)
+reviewer_1.rate_hw(student_1, 'Python', 10)
+
+reviewer_2.rate_hw(student_2, 'Python', 10)
+reviewer_2.rate_hw(student_2, 'Python', 7)
+reviewer_2.rate_hw(student_2, 'Python', 9)
+reviewer_2.rate_hw(student_2, 'Python', 8)
+
+stud_list = []
+stud_list.append(student_1)
+stud_list.append(student_2)
+
+lecture_list = []
+lecture_list.append(lecture_1)
+lecture_list.append(lecture_2)
+
+print(aversge_grade_students(stud_list, 'Python'))
+print(aversge_grade_lectures(lecture_list, 'Python'))
